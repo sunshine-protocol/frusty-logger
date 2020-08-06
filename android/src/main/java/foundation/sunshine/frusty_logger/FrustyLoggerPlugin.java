2cb4b76bd45d8fe58784709adf0092b6a8d8906d
Shady Khalifa <shekohex@gmail.com>
2020-08-06 15:58:01 +0200

    remove unwanted handlers

--- android/src/main/java/foundation/sunshine/frusty_logger/FrustyLoggerPlugin.java
+++ android/src/main/java/foundation/sunshine/frusty_logger/FrustyLoggerPlugin.java
@@ -1,7 +1,6 @@
 package foundation.sunshine.frusty_logger;
 
 import androidx.annotation.NonNull;
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -9,41 +8,44 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+
 /** FrustyLoggerPlugin */
 public class FrustyLoggerPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
+  /// The MethodChannel that will the communication between Flutter and native
+  /// Android
   ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+  /// This local reference serves to register the plugin with the Flutter Engine
+  /// and unregister it when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
 
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "frusty_logger");
+  public void
+  onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(
+        flutterPluginBinding.getFlutterEngine().getDartExecutor(),
+        "frusty_logger");
     channel.setMethodCallHandler(this);
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+  // This static function is optional and equivalent to onAttachedToEngine. It
+  // supports the old pre-Flutter-1.12 Android projects. You are encouraged to
+  // continue supporting plugin registration via this function while apps
+  // migrate to use the new Android APIs post-flutter-1.12 via
+  // https://flutter.dev/go/android-project-migration.
   //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
+  // It is encouraged to share logic between onAttachedToEngine and registerWith
+  // to keep them functionally equivalent. Only one of onAttachedToEngine or
+  // registerWith will be called depending on the user's project.
+  // onAttachedToEngine or registerWith must both be defined in the same class.
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "frusty_logger");
+    final MethodChannel channel =
+        new MethodChannel(registrar.messenger(), "frusty_logger");
     channel.setMethodCallHandler(new FrustyLoggerPlugin());
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
+    result.notImplemented();
   }
 
   @Override
